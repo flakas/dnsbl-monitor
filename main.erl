1202af3f1393f3a2830ab5c03c4508390df03ff5
Tautvidas Sipavičius <t.9b5as@gmail.com>
2018-10-12 21:25:57 +0300

    Add parallel processing of blacklist checks

--- main.erl
+++ main.erl
@@ -3,10 +3,44 @@
 
 -import(checker, [is_blacklisted/1]).
 
-check_all() ->
-  case file:read_file("ips.txt") of
+check_all(File, Workers) ->
+  case file:read_file(File) of
     {ok, Contents} ->
       IPs = string:split(string:trim(binary:bin_to_list(Contents)), "\n", all),
-      [{IP, checker:is_blacklisted(IP)} || IP <- IPs];
+      check_ips(IPs, Workers);
     _ -> error
   end.
+
+check_ips(IPs, Workers) ->
+  Chunks = split_list_into_chunks(IPs, Workers),
+  [spawn(?MODULE, check_ip_list, [{self(), Chunk}]) || Chunk <- Chunks],
+  Answers = get_answers(Workers, []),
+  lists:flatten(Answers).
+
+split_list_into_chunks(List, Chunks) ->
+  Indexed = lists:zip(lists:seq(1, length(List)), List),
+  [lists:filtermap(fun({Index, E}) ->
+                       case Index rem Chunks + 1 == Nth of
+                         true -> {true, E};
+                         false -> false
+                       end
+                   end,
+                   Indexed)
+   || Nth <- lists:seq(1, Chunks)].
+
+get_answers(0, Answers) ->
+  Answers;
+get_answers(Remaining, Answers) when Remaining > 0 ->
+  receive
+    {ok, Answer} ->
+      get_answers(Remaining - 1, [Answer | Answers]);
+    {error, _} -> get_answers(Remaining - 1, Answers)
+  end.
+
+check_ip_list({From, IPs}) ->
+  check_ip_list({From, IPs, []});
+check_ip_list({From, [], Results}) ->
+  From ! {ok, Results};
+check_ip_list({From, [IP|IPs], Results}) ->
+  Result = checker:is_blacklisted(IP),
+  check_ip_list({From, IPs, [{IP, Result}|Results]}).
